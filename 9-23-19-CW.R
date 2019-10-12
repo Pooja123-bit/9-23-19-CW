b818a2afcbd3eda5d2abebc3b64268131baf650c
Pooja123-bit <poojaupadhyay100@gmail.com>
2019-10-12 13:30:45 -0500

    k

--- 9-23-19-CW.R
+++ 9-23-19-CW.R
@@ -1,13 +1,35 @@
-d = read.table(file='ISIIS201405291242.txt', skip = 10, header = T, sep = "\t")
-stringsAsfactors=FALSE, quote="\",check.names=FALSE,
-na.strings="9999.99"
+getwd()
+setwd("C:/Users/pooja/OneDrive/Desktop/R/9-23-19-CW")
+
+list.files(pattern="txt")
+OR
+list.files(pattern=".txt")
+
+list.files()
+
+d = read.table(file='ISIIS201405291242.txt', sep="\t", skip = 10, header = T, 
+stringsAsFactors=FALSE, quote="\"",check.names=FALSE,
+na.strings="9999.99")
+
+d = read.table(file='ISIIS201405291242.txt', sep="\t", skip = 10, header = T,
+               fileEncoding = "ISO-8859-1", encoding = "UTF-8", 
+               stringsAsFactors=FALSE, quote="\"",check.names=FALSE,
+               na.strings="9999.99")
+
+names(d)
+head(d)
+
+
+
 file<-list.files(full.names=TRUE, pattern="ISIIS")
 file<-as.character(file)
 
 library(stringr)
 
-date<-scan(file, what="character", skip=1, nlines=1, quiet=TRUE)
+date<-scan(file="ISIIS201405291242.txt", what="character", 
+           skip=1, nlines=1, quiet=TRUE)
 date<-date[2]
+
 mm<-str_sub(string=date, start=1, end=2)
 dd<-str_sub(string=date, start=4, end=5)
 dd<-as.numeric(dd)
@@ -28,9 +50,10 @@ options("digits.secs"=3)
 d$dateTime<-str_c(d$date, d$time, sep=" ")
 
 d$dateTime<-as.POSIXct(strptime(x=d$dateTime, 
-format="%m/%d/%y %H:%M:%S",tz='America/New_York'))
+format="%m/%d/%y %H:%M:%S",tz="America/New_York"))
 
-d$dateTime<-d$dateTime-time.zone.change * 3600
+#to be able to alter timezone back to the one to initial
+d$dateTime<-d$dateTime-time.zone.change*3600
 
 d$changetime<-format(d$dateTime, tz<- 'America/Los_Angeles')
 
@@ -40,10 +63,23 @@ d$changetime<-format(d$dateTime, tz<- 'America/Los_Angeles')
 
 install.packages("openxlsx")
 library(openxlsx)
+getwd()
+setwd("C:/Users/pooja/OneDrive/Desktop/R/9-23-19-CW")
+list.files()
 
-openXL(file="import_datasets/Aurelia_SEAMAP_2012_2018_30mincell.xlsx")
+load(file="aurelia_15minCell_statareas.Rdata")
 
-d<-read.table(file="import_data")
+#Installing packages for text, csv and excel file
+install.packages("data.table")
+library(data.table)
+library(tidyverse)
+library(readxl)
+
+
+e<-read_excel(path="Aurelia_SEAMAP_2012-2018_30minCell.xlsx")
+
+read_xlsx(path="Aurelia_SEAMAP_2012-2018_30minCell.xlsx")
+
 
 9-25-2019
 
@@ -77,6 +113,25 @@ a.df=as.data.frame(a)
 a.f$ID=row.names(a)
 
 a2.df=data.frame(tid=row.names(a),mean=as.numeric(a))
+
 b=df %-% group_by(teamID) %-% summarise(totalHits=sum(H))
+
 c=aggregate(s=df$H, by= list(df$teamID, FUN=sum))
+
+#merge---
+q= data.frame(id=c('a','b','c','d'),num=c(1,2,3,4))
+v= data.frame(id=c('a','b','e'),car=c(6,7,8))
+
+qv=merge(x=q,y=v,by='id')
+qv
+
+#Include all values of x
+qv=merge(x=q,y=v,by='id', all.x=T) 
+
+#Include all values of y
+qv=merge(x=q,y=v,by='id', all.y=T)
+
+
+
+
 
